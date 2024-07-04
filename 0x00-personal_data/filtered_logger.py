e8e9fb19f84d6d79588541f16d37359e71dc1e46
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-07-04 11:50:40 +0300

    no db name

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -59,5 +59,5 @@ def get_db() -> MySQLConnection:
         user=getenv('PERSONAL_DATA_DB_USERNAME', "root"),
         password=getenv('PERSONAL_DATA_DB_PASSWORD', ""),
         host=getenv('PERSONAL_DATA_DB_HOST', "localhost"),
-        database=getenv('PERSONAL_DATA_DB_NAME', "holberton")
+        database=getenv('PERSONAL_DATA_DB_NAME')
         )

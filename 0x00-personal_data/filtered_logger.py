66fd02f8072ce3ab8b59776d2782c798c90338cd
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-07-04 09:44:25 +0300

    type annotation correct

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,12 +2,12 @@
 """ module to define a function called filter_datum
 that returns the log message obfuscated:
 """
+from typing import List
 import re
 
 
-def filter_datum(fields: str, redaction: str, message: str, separator: str) -> str:
-        """ function that returns the log message obfuscated """
-        for field in fields:
-                message = re.sub(rf'{field}=.*?{separator}',
-                    f'{field}={redaction}{separator}', message)
-        return message
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """ function that returns the log message obfuscated """
+    for field in fields:
+        message = re.sub(rf'{field}=.*?{separator}',f'{field}={redaction}{separator}', message)
+    return message

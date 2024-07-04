ba618d21df6ec9671b22a4cb1087c2c273fc3975
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-07-04 12:21:18 +0300

    is valid hashed password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,5 +7,14 @@ import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """ function that returns a salted, hashed password, which is a byte string """
+    """ function that returns a salted, hashed password,
+    which is a byte string """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ validate that the provided password matches the hashed password. """
+    return bcrypt.checkpw(
+        password=password.encode(),
+        hashed_password=hashed_password
+        )

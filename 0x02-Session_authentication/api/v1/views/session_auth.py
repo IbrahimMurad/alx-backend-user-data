398f658c36be3b27f9d7d6046ea0275a7dce8cda
IbrahimMurad <ibrahimmorad31@gmail.com>
2024-07-10 18:42:46 +0300

    style

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -15,15 +15,15 @@ def auth_session_login() -> str:
     """
     email = request.form.get('email')
     if not email:
-        return jsonify({ "error": "email missing" }), 400
+        return jsonify({"error": "email missing"}), 400
     password = request.form.get('password')
     if not password:
-        return jsonify({ "error": "password missing" }), 400
+        return jsonify({"error": "password missing"}), 400
     user = User.search({'email': email})
     if not user:
-        return jsonify({ "error": "no user found for this email" }), 404
+        return jsonify({"error": "no user found for this email"}), 404
     if not user[0].is_valid_password(password):
-        return jsonify({ "error": "wrong password" }), 401
+        return jsonify({"error": "wrong password"}), 401
     user = user[0]
     from api.v1.app import auth
     session_id = auth.create_session(user.id)
@@ -33,7 +33,11 @@ def auth_session_login() -> str:
     return res
 
 
-@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+@app_views.route(
+    '/auth_session/logout',
+    methods=['DELETE'],
+    strict_slashes=False
+)
 def logout() -> str:
     """ DELETE /api/v1/auth_session/login
     Return:

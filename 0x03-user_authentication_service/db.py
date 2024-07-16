339730b70669725276457175758416955bdaff07
IbrahimMurad <ibrahimmorad31@gmail.com>
2024-07-16 21:27:29 +0300

    #!/usr/bin/env python3

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,11 +1,13 @@
-"""DB module
+#!/usr/bin/env python3
+""" DB module
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
-from user import Base, User
+from user import Base
+from user import User
 
 
 class DB:

cbaf8845cb507dbfd5e5e3a35711ad01b0b2e98c
IbrahimMurad <ibrahimmorad31@gmail.com>
2024-07-17 08:21:59 +0300

    register a user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """ This is flask app
 """
-from flask import Flask, jsonify
+from auth import Auth
+from flask import Flask, jsonify, request
 
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -13,6 +15,23 @@ def basic() -> str:
     Return: a JSON payload: {"message": "Bienvenue"}
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """ POST /users 
+    an end-point to register a user
+    Returns: a JSON payload: {"email": "<email>", "message": "user created"}
+    Or: a JSON payload: {"message": "email already registered"} with a 400 status code
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if email and password:
+        try:
+            user = AUTH.register_user(email,password)
+            return jsonify({"email": user.email, "message": "user created"})
+        except ValueError:
+            return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":

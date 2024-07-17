ab19ff311250fd7a2fc0977af78fce9828ca11f5
IbrahimMurad <ibrahimmorad31@gmail.com>
2024-07-17 14:58:41 +0300

    fix

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -45,7 +45,7 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-            return checkpw(password.encode(), user.hashed_password, )
+            return checkpw(password.encode(), user.hashed_password)
         except NoResultFound:
             return False
 
